dd834daded13e53b687171055c744311e435bd15
JohanRodriguez007 <150948505+JohanRodriguez007@users.noreply.github.com>
2024-10-08 20:29:37 -0500

    Mejoras Diseño P1

--- inc/footer.php
+++ inc/footer.php
@@ -1,17 +1,35 @@
-<footer class="personal-info">
-       
-       <div class="container-info">
+<footer class="text-center text-lg-start personal-info">
+<section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <div class="me-5 d-none d-lg-block text-white">
+      <i class="fas fa-wallet" style="margin-right: 10px;"></i>
+      <span>Nuestro Nequi es 314 4851640</span>
+    </div>
+</section>
 
-           <div class="info">
+  <section class="">
+    <div class="container-info text-center text-md-start mt-5">
+      <div class="row mt-3">
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4 text-white">
+            Cigarrería Donde Deya
+          </h6>
+          <p class="text-white">
+          Descubre todo lo que necesitas en nuestra cigarrería: desde cervezas y licores hasta deliciosos mecatos y productos de consumo diario.
+          </p>
+        </div>
 
-               <p>Direccion: Calle 127 abis No 90b-30</p>
-               <p>Contacto: 311 391 1473</p>
-               <p>Correo: linita5marcelita@gmail.com</p>
-               <p>&copy;2024 Cigarrería Donde Deya, Derechos Reservados</p>
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold mb-4 text-white">Contacto</h6>
+          <p class="text-white"><i class="fas fa-home me-3"></i> Calle 127 abis No 90b-30</p>
+          <p class="text-white"><i class="fas fa-envelope me-3"></i> linita5marcelita@gmail.com</p>
+          <p class="text-white"><i class="fas fa-phone me-3"></i> 314 4851640</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05); color: #ffffff;">
+    &copy;2024 Cigarrería Donde Deya, Derechos Reservados
+  </div>
+</footer>
 
 
-           </div>
-       </div>
-
-       
-   </footer>
--- inc/head.php
+++ inc/head.php
@@ -4,4 +4,7 @@
  <link rel="icon" href="./img/icono.png">
 <link rel="stylesheet" href="">
 <link rel="stylesheet" href="./css/estilos2.css">
+<link rel="stylesheet" href="./css/estilos_footer_V2.css">
 <link rel="stylesheet" href="./css/bulma.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
